6b7d4b22999c0a0bc6b9179d284e4d09468c0f04
Skyyler Siejko <skyyconnected@gmail.com>
2020-11-03 03:06:09 -0800

    seperation of class Deck and Card not working

--- engine/SpellSlingers/SpellSlingers/ContentView.swift
+++ engine/SpellSlingers/SpellSlingers/ContentView.swift
@@ -10,31 +10,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var cardIndexA = 2
-    @State private var cardIndexB = 2
+
     @State  private var POWERA = 0
     @State private var POWERB = 0
     @State private var CURRENT_COLOR = "_blue"
-    @State private var CARDS = ["spell", "spell", "spell", "recycle", "empty", "resource","cancel"]
-    @State private var CARD_POWER = [1,2,3, 0, 0,1,3]
-    @State private var cards:Array<Card> = []
+    var deck = Deck(count_:64, owner_:"_blue")
+   
    
     
     
-    
-   func getRandomCard() -> Card?{
-    let id = Int.random(in: 0...CARDS.count-1);
-    return Card(_id: id, name: CARDS[id], power: CARD_POWER[id],
-        owner: CURRENT_COLOR)
-    }
+
     
     
    
     var body: some View {
         
+   
        
-        
         ZStack{
+            
            
             Image("background")
                 .resizable()
@@ -48,20 +42,10 @@ struct ContentView: View {
              
                    
                    HStack{
-                        
-                                         
-                      if(self.cards.count > 0){
-                          
-                        ZStack(alignment: .leading) {
-                          ForEach(self.cards, id: \.self) { card in
-                              Card(_id: card._id, name: card.name,power:card.power, owner:card.owner )
-                               
-                               
-                             }
-                             Spacer()
-                         }
-                         
-                      }
+                      Text("HASTACK")
+                    Text(String(self.deck.cards.count))
+                    
+                     
                       
                   }
                 
@@ -72,21 +56,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
             
-            /*
-             
-             Button(action: {
-             
-             handler logic
-             }){
-             
-             UI rerender With
-             }
-             
-             
-             
-             
-             */
-                
+          
                 
                 
                 Button(action:{
@@ -95,16 +65,18 @@ struct ContentView: View {
                     }else{
                          self.CURRENT_COLOR = "_blue"
                     }
-                 
+                    self.deck.create();
                     
-                        
+                    
+                     /*
                         let id = Int.random(in: 0...self.CARDS.count-1);
                         self.cards.append( Card(_id: id, name: self.CARDS[id],  power: self.CARD_POWER[id],owner: self.CURRENT_COLOR))
                     
-                    
+                    */
                   
                     
                 }){
+                    
                     
                    Image("back")
                     .renderingMode(.original)
@@ -140,94 +112,6 @@ struct ContentView: View {
        
         
     }
-
-
-
-
-
-struct Card: View, Hashable {
-    var _id: Int
-    var name: String
-    var power: Int
-    var owner: String
-    
-    init(_id:Int, name: String, power: Int, owner:String){
-        self._id = _id
-        self.name = name
-        self.power = power
-        self.owner = owner
-    }
-    
-  
-    
-        
-  var body: some View {
-    
-    
-    Image(self.name+self.owner)
-    .renderingMode(.original)
-    .resizable()
-    .frame(width: 150, height:175, alignment: .center)
-    .cornerRadius(30.0)
-    .padding(0)
-    
-    }
-            
-}
-
-
-    
-
-
-    
-    
-/*func drawOnImage(_image: UIImage) -> UIImage{
-         
-         // Create a context of the starting image size and set it as the current one
-         UIGraphicsBeginImageContext(_image.size)
-         
-         // Draw the starting image in the current context as background
-         _image.draw(at: CGPoint.zero)
-
-         // Get the current context
-         let context = UIGraphicsGetCurrentContext()!
-
-         // Draw a red line
-         context.setLineWidth(2.0)
-         context.setStrokeColor(UIColor.red.cgColor)
-         context.move(to: CGPoint(x: 100, y: 100))
-         context.addLine(to: CGPoint(x: 200, y: 200))
-         context.strokePath()
-         
-         // Draw a transparent green Circle
-         context.setStrokeColor(UIColor.green.cgColor)
-         context.setAlpha(0.5)
-         context.setLineWidth(10.0)
-         context.addEllipse(in: CGRect(x: 100, y: 100, width: 100, height: 100))
-         context.drawPath(using: .stroke) // or .fillStroke if need filling
-         
-         // Save the context as a new UIImage
-        
-         
-         // Return modified image
-        
-    }*/
-
-
-
-    
-    
-    
-
-
-
-
-class Deck {
-    
-}
-
-
-
 
 
 struct ContentView_Previews: PreviewProvider {
---
+++ engine/SpellSlingers/SpellSlingers/Deck.swift
@@ -0,0 +1,85 @@
+//
+//  Deck.swift
+//  SpellSlingers
+//
+//  Created by Skyyler Siejko on 11/3/20.
+//  Copyright © 2020 Skyyler Siejko. All rights reserved.
+//
+
+import Foundation
+
+import SwiftUI
+
+//Card : ID, name, power, owner
+struct Deck: View{
+   
+    
+    var count: Int
+    var owner: String
+    @State public var cards:Array<Card> = []
+    @State private var CARD_POWER = [1,2,3, 0, 0,1,3]
+    @State private var CARD_AMOUNT = [4,4,4, 4, 4,40,4]
+    @State private var CARDS = ["spell", "spell", "spell", "recycle", "empty", "resource","cancel"]
+   
+    
+    init(count_:Int, owner_:String){
+        self.owner = owner_
+        self.count = count_
+            }
+    
+    
+    func create() -> Array<Card>{
+        for cardIndex in 0...CARDS.count-1{
+            for _ in 0...CARD_AMOUNT.count-1{
+              
+                    self.cards.append(Card(_id:cardIndex,name: CARDS[cardIndex]+self.owner, power: CARD_POWER[cardIndex], owner: self.owner ))
+                
+            }
+        }
+        
+       
+        
+        return self.cards
+    }
+    
+    
+    func getRandomCard() -> Card?{
+        let id = Int.random(in: 0...CARDS.count-1);
+        return Card(_id: id, name: CARDS[id], power: CARD_POWER[id],
+                    owner: self.owner)
+    }
+    
+    
+
+    
+  
+    
+        
+  var body: some View {
+     Group{
+        Text(String(self.cards.count))
+       if(self.cards.count > 0){
+           ZStack{
+               ForEach(self.cards, id: \.self) { card in
+                   
+                   Card(_id: card._id, name: card.name,power:card.power, owner:card.owner )
+                   
+                   }
+                                         
+                                         
+           }
+       }
+
+        
+    }
+    
+   /* Image(self.name+self.owner)
+    .renderingMode(.original)
+    .resizable()
+    .frame(width: 150, height:175, alignment: .center)
+    .cornerRadius(30.0)
+    .padding(0)*/
+    
+    }
+            
+}
